bf4bee7358631a89b66c2e39d680d3c7a343a113
Юлия <yulentsiyaa@mail.ru>
2022-04-07 07:47:45 +0300

    fix comment

--- lesson07/task01.js
+++ lesson07/task01.js
@@ -1,12 +1,12 @@
 'use strict'
 
-// const filter = (arrStudents, arrFailedStudents) => {
-//   let findGoodExams = arrStudents.filter(item => !arrFailedStudents.includes(item));
+const filter = (arrStudents, arrFailedStudents) => {
+  let findGoodExams = arrStudents.filter(item => !arrFailedStudents.includes(item));
   
-//   return findGoodExams;
-// }
+  return findGoodExams;
+}
 
-// const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов'];
-// const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
+const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов'];
+const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
 
-// console.log(filter(allStudents, failedStudents));
+console.log(filter(allStudents, failedStudents));
